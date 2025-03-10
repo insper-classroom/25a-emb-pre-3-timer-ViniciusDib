2360984f7334b9bc777f900cc53d16604e0325d6
Vinicius Dib <viniciusdib15@gmail.com>
2025-03-10 11:39:23 -0400

    ultimo tst

--- exe3/main.c
+++ exe3/main.c
@@ -6,6 +6,20 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile int alarm = 0;
+volatile bool timer_fired = false;
+
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    if(!alarm){
+        timer_fired = true;
+        return 0;
+    }
+    else{
+        timer_fired = false;
+        return 0;
+    }
+
+}
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
@@ -15,6 +29,7 @@ void btn_callback(uint gpio, uint32_t events) {
     } else if (events == 0x8) { // rise edge
         if (gpio == BTN_PIN_R)
             flag_f_r = 0;
+            alarm = 1;
     }
 }
 
@@ -31,9 +46,21 @@ int main() {
     gpio_set_irq_enabled_with_callback(
         BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
 
+
+
     while (true) {
 
         if (flag_f_r) {
+            alarm_id_t alarm = add_alarm_in_ms(300, alarm_callback, NULL, false);
+            flag_f_r =0;
+            alarm =0;
+
+        }
+        if(timer_fired){
+            gpio_put(LED_PIN_R,1);
+            sleep_ms(500);
+            gpio_put(LED_PIN_R, 0);
+            timer_fired = false;
         }
     }
 }
