e1232fa6bda4fa91c767547102e51bd1248eb37e
Vinicius Dib <viniciusdib15@gmail.com>
2025-03-10 11:12:08 -0400

    mais um

--- exe2/main.c
+++ exe2/main.c
@@ -7,86 +7,95 @@
 #define LED_PIN_R 4
 #define LED_PIN_G 6
 
+volatile bool btn_r = false;
+volatile bool btn_g = false;
 volatile bool timer_ativo_r = false;
 volatile bool timer_ativo_g = false;
-volatile bool flag_r = false;
-volatile bool flag_g = false;
-repeating_timer_t timer_r;
-repeating_timer_t timer_g;
 
-
-bool timer_callback_r(repeating_timer_t *rt) {
-    gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R)); 
+bool timer_callback_r(repeating_timer_t *rt)
+{
+    timer_ativo_r = true;
+    return true;
+}
+bool timer_callback_g(repeating_timer_t *rt)
+{
+    timer_ativo_g = true;
     return true;
 }
 
-
-bool timer_callback_g(repeating_timer_t *rt) {
-    gpio_put(LED_PIN_G, !gpio_get(LED_PIN_G)); 
-    return true;
-}
-
-void btn_callback(uint gpio, uint32_t events) {
-    if (events & GPIO_IRQ_EDGE_FALL) {
+void btn_callback(uint gpio, uint32_t events)
+{
+    if (events & GPIO_IRQ_EDGE_FALL)
+    {
         if (gpio == BTN_PIN_R)
-            flag_r = true;
-        else if (gpio == BTN_PIN_G)
-            flag_g = true;
+            btn_r = !btn_r;
+        if (gpio == BTN_PIN_G)
+            btn_g = !btn_g;
     }
 }
 
+int main()
+{
+    repeating_timer_t timer;
+    repeating_timer_t timer2;
 
-
-int main() {
     stdio_init_all();
-    
 
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
     gpio_put(LED_PIN_R, 0);
-    
-    gpio_init(LED_PIN_G);
-    gpio_set_dir(LED_PIN_G, GPIO_OUT);
-    gpio_put(LED_PIN_G, 0);
-
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
 
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+
+    
+    gpio_init(LED_PIN_G);
+    gpio_set_dir(LED_PIN_G, GPIO_OUT);
+    gpio_put(LED_PIN_G, 0);
+
     gpio_init(BTN_PIN_G);
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
-    
 
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
-    
-    while (true) {
-        if (flag_r) {
-            flag_r = false;
-            if (!timer_ativo_r) {
-                add_repeating_timer_ms(500, timer_callback_r, NULL, &timer_r);
-                timer_ativo_r = true;
-            } else {
-                cancel_repeating_timer(&timer_r);
+
+    if (!add_repeating_timer_ms(500, timer_callback_r, NULL, &timer))
+    {
+        printf("Failed to add timer/n");
+    }
+
+    if (!add_repeating_timer_ms(250, timer_callback_g, NULL, &timer2))
+    {
+        printf("Failed to add timer/n");
+    }
+    while (true)
+    {
+        if (btn_r)
+        {
+            if (timer_ativo_r)
+            {
+                gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R));
                 timer_ativo_r = false;
-                gpio_put(LED_PIN_R, 0);
             }
         }
-        
-        if (flag_g) {
-            flag_g = false;
-            if (!timer_ativo_g) {
-                add_repeating_timer_ms(250, timer_callback_g, NULL, &timer_g);
-                timer_ativo_g = true;
-            } else {
-                cancel_repeating_timer(&timer_g);
+        else
+        {
+            gpio_put(LED_PIN_R, 0);
+        }
+        if (btn_g)
+        {
+            if (timer_ativo_g)
+            {
+                gpio_put(LED_PIN_G, !gpio_get(LED_PIN_G));
                 timer_ativo_g = false;
-                gpio_put(LED_PIN_G, 0);
             }
         }
-        
-        sleep_ms(10);
+        else
+        {
+            gpio_put(LED_PIN_G, 0);
+        }
     }
 }
