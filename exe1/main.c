64f47a22589ba2ff2b313edafe449a47acba1341
Vinicius Dib <viniciusdib15@gmail.com>
2025-03-10 09:55:31 -0400

    tst_1

--- exe1/main.c
+++ exe1/main.c
@@ -2,34 +2,53 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const int BTN_PIN_R = 28;
-const int LED_PIN_R = 4;
+#define BTN_PIN_R 28
+#define LED_PIN_R 4
 
-volatile int flag_f_r = 0;
+volatile bool timer_ativo = false;
+volatile bool btn = false;
+repeating_timer_t timer;
+
+bool timer_callback(repeating_timer_t *rt) {
+    gpio_put(LED_PIN_R, !gpio_get(LED_PIN_R)); 
+    return true; 
+}
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        flag_f_r = 1;
-    } else if (events == 0x8) { // rise edge
+    if (events & GPIO_IRQ_EDGE_FALL) {
+        btn = true;
     }
 }
 
 int main() {
     stdio_init_all();
+    
+
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
+    gpio_put(LED_PIN_R, 0); 
+
 
     gpio_init(BTN_PIN_R);
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
+    
 
-    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback);
-
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+    
     while (true) {
-
-        if (flag_f_r) {
-            flag_f_r = 0;
+        if (btn) {
+            btn = false; 
+            
+            if (!timer_ativo) {
+                add_repeating_timer_ms(500, timer_callback, NULL, &timer);
+                timer_ativo = true;
+            } else {
+                cancel_repeating_timer(&timer);
+                timer_ativo = false;
+                gpio_put(LED_PIN_R, 0);
+            }
         }
+        sleep_ms(10); 
     }
 }
